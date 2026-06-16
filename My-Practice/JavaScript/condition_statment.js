5e90f6001a7f66b156f0047b567c9cbb90ccd4fd
sahranhameed.it@gmail.com <sahranhmd2000@gmail.com>
2026-06-16 18:01:34 +0300

    add condition_statment files

--- My-Practice/JavaScript/condition_statment.js
+++ My-Practice/JavaScript/condition_statment.js
@@ -1,4 +1,4 @@
-/*
+
 if(true) 
     {
         console.log("This is true");
@@ -7,14 +7,40 @@ else
     {
         console.log("This is false");
     }
-*/
 
+
+// 01. Is it raining outside?//
+/*
 var rain = true;
     if(rain){
         console.log("It is raining Take an umbrella");
     
     }
-
     else{
         console.log("It is not raining, you can go outside");
     }
+*/
+
+//02. Did You Finish Your Homework?//
+/*
+var homework = false;
+    if(homework)
+        {
+            console.log("Great Job!")
+        }
+    else{
+            console.log("Finish your Homework before playing")
+        }
+*/
+
+// 03.Are there any cookies left?//
+/*
+var cookies = true;
+    if(cookies)
+        {
+            console.log("Would you like a cookie?")
+        }
+    else{
+            console.log("Time to bake more Cookies")
+        }
+*/
