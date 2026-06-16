03dae4c9be5d652000dc985debd328579348a845
sahranhameed.it@gmail.com <sahranhmd2000@gmail.com>
2026-06-16 22:22:47 +0300

    prectice files upload

--- My-Practice/JavaScript/operators.js
+++ My-Practice/JavaScript/operators.js
@@ -1,12 +1,72 @@
-//Pre increment operator example//
- var a = 10
- var b = ++a
- console.log(b)
 
+// Logical AND operator
+console.log(true && true); // true (if only all true - all the function is true)
+console.log(true && false); // false (if one false - all the function is false)
+
+// Logical OR Operator
+console.log(true || true); // true  } (if only one is true - all the function is true)
+console.log(false || true); // true } 
+
+// Logical NOT operator
+console.log(!true); // false } Working is oposit dieraction
+console.log(!false); // true }
+
+
+
+// If else Exercise //
+var season = "winter";
+    if(season == "spring")
+    {
+        console.log("Enjoy the blooming floweres.")
+    }
+
+    if(season == "summer")
+    {
+          console.log("Have fun in the Sun.")
+    }
+
+        if(season == "autumn")
+    {
+          console.log("Admire the colorful leaves.")
+    }
+
+    if(season == "winter")
+    {
+          console.log("Bundle up and stay warm.")
+    }
+
+
+
+// Odd number and Even number//
+    var number = ("odd")
+    if(number == "even")
+    {
+          console.log("The number is even")
+    }
+    else
+        {
+            console.log("The number is odd")
+        }
+
+
+// For Loop //
+for(count=1; count<=10; count=count+1)
+{
+    console.log(count) // Print Numbers 1 to 10
+    console.log("Sahran") // 10 Times Print Names
+}
+
+
+// For Loop //
+for(count=1; count<=10; count=count+2)
+{
+    console.log(count) // Print Numbers 1 to 10, but Increment bt 2 (Odd Number)
  
-/*
-//Post increment operator example//
- var a = 10
- var b = a++
- console.log(a)
-*/
+}
+
+
+for(count=10; count<=10; count=count+1)
+{
+    console.log(count) // Print Numbers 1 to 10, but Increment bt 2 (Odd Number)
+ 
+}
