eb26b88740c6f1f87372a2c3e55826c4651e93f3
sahranhameed.it@gmail.com <sahranhmd2000@gmail.com>
2026-06-19 19:04:33 +0300

    Task01_Arithmetic Operators and  files upload

--- My-Task/01. Java Operators and Control Flow Statements/Task-01/ArithmeticOperators.java
+++ My-Task/01. Java Operators and Control Flow Statements/Task-01/ArithmeticOperators.java
@@ -6,10 +6,8 @@ import java.util.Scanner;
  * of two numbers entered by the user.
 */
 
-public class ArithmeticOperators 
-{     
-    public static void main(String args[])
-    {
+public class ArithmeticOperators {
+    public static void main(String args[]) {
         try (Scanner input = new Scanner(System.in))
 
         // ---------- Input Section ----------
@@ -20,30 +18,27 @@ public class ArithmeticOperators
             System.out.println("Enter the Second Number: ");
             double secondNumber = input.nextDouble();
 
-    // ------------- Calculation Section -------------//
+            // ------------- Calculation Section -------------//
 
             double addition = firstNumber + secondNumber;
             double subtraction = firstNumber - secondNumber;
             double multiplication = firstNumber * secondNumber;
 
-    // ------------- Output Section -------------//
-    
+            // ------------- Output Section -------------//
+
             System.out.println("\n------------ Results ------------");
-            System.out.println("Addition: " + firstNumber + " + " + secondNumber + " = " + addition);   
+            System.out.println("Addition: " + firstNumber + " + " + secondNumber + " = " + addition);
             System.out.println("Subtraction: " + firstNumber + " - " + secondNumber + " = " + subtraction);
             System.out.println("Multiplication: " + firstNumber + " * " + secondNumber + " = " + multiplication);
 
-     // Division — zero check //
-     if (secondNumber !=0) 
-        {   
-            double division = firstNumber / secondNumber;
-            System.out.println("Division: " + firstNumber + " / " + secondNumber + " = " + division);
-        }
-    else
-       {
-            System.out.println("Division: Cannot divide by zero!");
-       }
+            // Division — zero check //
+            if (secondNumber != 0) {
+                double division = firstNumber / secondNumber;
+                System.out.println("Division: " + firstNumber + " / " + secondNumber + " = " + division);
+            } else {
+                System.out.println("Division: Cannot divide by zero!");
+            }
             System.out.println("--------------------------------------------");
-        }            
+        }
     }
 }
