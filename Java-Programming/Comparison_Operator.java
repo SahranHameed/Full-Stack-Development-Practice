b25fdcf195945d7a02db7fdffcd44e7554aaea76
sahranhameed.it@gmail.com <sahranhmd2000@gmail.com>
2026-06-24 16:05:48 +0300

    if else statment practice file upload

---
+++ Java-Programming/Comparison_Operator.java
@@ -0,0 +1,3 @@
+public class Comparison_Operator {
+    
+}
---
+++ Java-Programming/Statments.java
@@ -0,0 +1,14 @@
+public class Statments {
+    public static void main(String args[])
+
+    {
+        boolean rain = true;
+        if (rain) {
+            System.out.println("Take an Umbrella");
+        }
+        else
+        {
+            System.out.print("Enjoy the sunlight");
+        }
+    }
+}
