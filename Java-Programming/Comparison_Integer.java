883614a7341082f8630c637fbd50efd775e1987e
sahranhameed.it@gmail.com <sahranhmd2000@gmail.com>
2026-06-24 17:25:30 +0300

    Add Comparison_Integer Practice Files

---
+++ Java-Programming/Comparison_Integer.java
@@ -0,0 +1,62 @@
+// Comparison Operator Greathen and Lessthen 
+public class Comparison_Integer {
+    public static void main(String args[])
+
+    {
+        int number1 = 40;
+        int number2 = 60;
+
+        if (number1 > number2) {
+            System.out.println("Number 1 is Greater");
+        }
+        else
+        {
+            System.out.print("Number 2 is Greater");
+        }
+    }
+}
+
+
+// Comparison_Operator Checking
+/*
+public class Comparison_Integer {
+    public static void main(String args[])
+
+    {
+        int number1 = 40;
+        int number2 = 40;
+
+        if (number1 == number2) {
+            System.out.println("Number 1 is Greater =");
+        } else {
+            System.out.print("Number 2 is Greater");
+        }
+    }
+}
+*/
+
+
+// How to Compare Two Intiger Numbers
+/*
+import java.util.Scanner;
+
+public class Comparison_Integer {
+    public static void main(String args[])
+
+    {
+        try (Scanner store = new Scanner(System.in)) {
+            System.out.println("Enter the Number1: ");
+            int number1 = store.nextInt();
+
+            System.out.println("Enter the Number2: ");
+            int number2 = store.nextInt();
+
+            if (number1 == number2) {
+                System.out.println("Both the Numbers are Equal");
+            } else {
+                System.out.print("Not Equal");
+            }
+        }
+    }
+}
+*/
