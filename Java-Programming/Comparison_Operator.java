9399001bc9c12e0ac92ec34d791ca5f35ded261f
sahranhameed.it@gmail.com <sahranhmd2000@gmail.com>
2026-06-24 16:15:26 +0300

    add Comparison Operator practice file

--- Java-Programming/Comparison_Operator.java
+++ Java-Programming/Comparison_Operator.java
@@ -1,3 +1,18 @@
+// Comparison Operator
 public class Comparison_Operator {
-    
+    public static void main(String args[])
+
+    {
+        int number1 = 40;
+        int number2 = 60;
+
+        if (number1 > number2) {
+            System.out.println("Number 1 is Greater");
+        }
+        else
+        {
+            System.out.print("Number 2 is Greater");
+        }
+    }
 }
+
