f8457b8a2ff1be51005faf9051e7e2244ab4edd4
sahranhameed.it@gmail.com <sahranhmd2000@gmail.com>
2026-06-28 02:20:24 +0300

    Task_03 Calculate sum of all elements file upload

--- My-Task/04. Java Arrays - Practical Tasks/Task-03/SumOfElements.java
+++ My-Task/04. Java Arrays - Practical Tasks/Task-03/SumOfElements.java
@@ -2,17 +2,22 @@
   Task 03 - Calculate the Sum of All Elements
 */
 
+import java.util.Arrays;
+
 public class SumOfElements {
     public static void main(String[] args) {
 
-        int[] numbers = {5, 13, 10, 2, 4, 5, 7};
-        int sum = 0;
+        // Create an array
+        int[] numbers = { 5, 13, 10, 2, 4, 5, 7 };
+        int sum = 0; // Initialize the sum variable
 
+        // Add each array element to the sum
         for (int i = 0; i < numbers.length; i++) {
-            sum += numbers[i];  // add each element to sum
+            sum += numbers[i]; // add each element to sum
         }
 
-        System.out.println("Array: [5, 13, 10, 2, 4, 5, 7]");
+        // Display the total sum
+        System.out.println("Array: " + Arrays.toString(numbers));
         System.out.println("Sum of Elements: " + sum);
     }
 }
