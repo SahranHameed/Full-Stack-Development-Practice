e0ef7edcfff182b82811df2e65b31f6dcf17de16
sahranhameed.it@gmail.com <sahranhmd2000@gmail.com>
2026-06-28 11:17:46 +0300

    Task_02 Check Whether a String is a Palindrome file upload

--- My-Task/06. Java Strings - Practical Tasks/Task-02/PalindromeString.java
+++ My-Task/06. Java Strings - Practical Tasks/Task-02/PalindromeString.java
@@ -1,3 +1,28 @@
+/*
+  Task 02 - Check Whether a String is a Palindrome
+  Compare characters from both ends toward center
+*/
 public class PalindromeString {
-    
+    public static void main(String[] args) {
+        String str = "madam"; // Change to test
+        boolean isPalindrome = true;
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("\"" + str + "\" is a Palindrome");
+        } else {
+            System.out.println("\"" + str + "\" is NOT a Palindrome");
+        }
+    }
 }
